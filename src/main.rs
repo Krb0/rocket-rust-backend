a5783651195ebe5f04a2e2605323dfbc6ec1ae34
Krb0 <maurodavidcaceres@gmail.com>
2022-12-28 17:53:15 -0300

    feat: :sparkles: Sqlite database added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod custom_routes;
 mod repository;
 use rocket_dyn_templates::Template;
 use custom_routes::{profile::profile_router, about::about_router};
+use std::fs;
 
 #[macro_use] extern crate rocket;
 
@@ -16,7 +17,10 @@ fn rocket() -> _ {
 
     let profile_routes = profile_router();
     let about_routes = about_router();
-
+    if fs::metadata("database/users.sqlite").is_err() {
+        println!("Creating database");
+        repository::initial_setup();
+    }
     rocket::build().mount("/", routes![index])
     .mount("/profile", profile_routes).mount("/about", about_routes).attach(Template::fairing())
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,12 +1,13 @@
 
-pub fn main(){
+pub fn initial_setup(){
 
-    let connection = sqlite::open("test.sqlite").unwrap();
-
-let query = "
-    CREATE TABLE users (name TEXT, age INTEGER);
-    INSERT INTO users VALUES ('Alice', 42);
-    INSERT INTO users VALUES ('Bob', 69);
-";
-connection.execute(query).unwrap();
+    let connection = sqlite::open("database/users.sqlite").unwrap();
+    let query = "
+        CREATE TABLE users (name TEXT, age INTEGER);
+        INSERT INTO users VALUES ('Juan', 10);
+        INSERT INTO users VALUES ('Pedro', 12);
+        INSERT INTO users VALUES ('Pepe', 92);
+        INSERT INTO users VALUES ('Roberto', 50);
+    ";
+    connection.execute(query).unwrap();
 }
